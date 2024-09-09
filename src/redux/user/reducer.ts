38bfca072cc35d593cd3db5267eba80871868397
Mudasir1406 <mudasir1406@gmail.com>
2024-09-10 04:00:39 +0500

    setup

---
+++ src/redux/user/reducer.ts
@@ -0,0 +1,30 @@
+import {PayloadAction} from '@reduxjs/toolkit';
+import {UserSliceType} from './slice';
+
+type stateType = UserSliceType;
+type actionType = PayloadAction<Partial<UserSliceType>>;
+
+export const setUser = (state: stateType, action: actionType) => {
+  const {
+    accessToken,
+    customerEmail,
+    customerName,
+    isLoggedIn,
+    drawerOpen,
+    customerId,
+    phoneno,
+    userCard,
+  } = action.payload;
+
+  state.accessToken =
+    accessToken !== undefined ? accessToken : state.accessToken;
+  state.customerEmail =
+    customerEmail !== undefined ? customerEmail : state.customerEmail;
+  state.customerName =
+    customerName !== undefined ? customerName : state.customerName;
+  state.isLoggedIn = isLoggedIn !== undefined ? isLoggedIn : state.isLoggedIn;
+  state.drawerOpen = drawerOpen !== undefined ? drawerOpen : state.drawerOpen;
+  state.customerId = customerId !== undefined ? customerId : state.customerId;
+  state.phoneno = phoneno !== undefined ? phoneno : state.phoneno;
+  state.userCard = userCard !== undefined ? userCard : state.userCard;
+};
---
+++ src/redux/user/slice.ts
@@ -0,0 +1,37 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {setUser} from './reducer';
+export type CardType = {
+  cardNumber: string;
+  exp: string;
+  cvc: string;
+  cardName: string;
+};
+export type UserSliceType = {
+  accessToken: string;
+  customerEmail: string;
+  customerName: string;
+  isLoggedIn: boolean;
+  drawerOpen: boolean;
+  customerId: string;
+  phoneno: string;
+  userCard: CardType | undefined;
+};
+
+export const userSliceIntialState: UserSliceType = {
+  accessToken: '',
+  customerEmail: '',
+  customerName: '',
+  isLoggedIn: false,
+  drawerOpen: false,
+  customerId: '',
+  phoneno: '',
+  userCard: undefined,
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: userSliceIntialState,
+  reducers: {setUser},
+});
+
+export const userActions = userSlice.actions;
